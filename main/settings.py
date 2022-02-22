f6154bee754c2369e98edef1475d7515d73896b3
tarnilok <tarnilok@gmail.com>
2022-02-23 01:24:12 +0300

    imported tkinter

--- main/settings.py
+++ main/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 from decouple import os
 import django_heroku
+import tkinter
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
