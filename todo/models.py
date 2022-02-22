405633eda3a6e22e8f360ca7c9aefa3d2e901400
tarnilok <tarnilok@gmail.com>
2022-02-23 01:45:49 +0300

    useless comments cleared

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
 class Group(models.Model):
     name=models.CharField(max_length=100, unique=True)
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import ListAPIView
 from .models import Task, Group
@@ -18,18 +16,6 @@ class TaskView(ModelViewSet):
     filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     ordering_fields = ['title', 'due_date', 'priority', 'user']
     filterset_fields = ['task_group']
-    
-    # http://127.0.0.1:8000/tasks/?task_group=24&ordering=-due_date
-    
-    def get(self, request, *args, **kwargs):
-        group_id = Group.objects.all()
-        print(group_id)
-        # print(self)
-        # groupName = Group.objects.filter()
-        # task = Task.objects.all()
-        # print(task)
-        # serializer = GroupSerializer(groupName, many=True)
-        # return Response(serializer.data)
     
 class UserView(ListAPIView):
     queryset = User.objects.all()
