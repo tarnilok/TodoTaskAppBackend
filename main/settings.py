3f577b7946ac50f36ee3f0207e7ca148ce382360
tarnilok <tarnilok@gmail.com>
2022-02-26 14:37:24 +0300

    static files folder empties

--- main/settings.py
+++ main/settings.py
@@ -136,14 +136,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')   
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-# STATIC_URL = 'static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#     ("js", os.path.join(STATIC_ROOT, 'js')),
-#     ("css", os.path.join(STATIC_ROOT, 'css')),
-#     ("images", os.path.join(STATIC_ROOT, 'images')),
-#     ("fonts", os.path.join(STATIC_ROOT, 'fonts')),
-# )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')   
 MEDIA_URL = '/media/' 
